3fc3fca3a0b6c16cfd36a62f72a73d508ad91ba0
LokeshGullapalli <lokeshchowdary7117@gmail.com>
2024-12-06 16:28:02 -0500

    Add requirements

--- app/main.py
+++ app/main.py
@@ -1,7 +1,3 @@
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
-
 import streamlit as st
 from langchain_community.document_loaders import WebBaseLoader
 from resource.chains import Chain
